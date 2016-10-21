4e5142e8251a0146e2ef69f6ebaf8cf7dddaf991
AkariInago <akariinago@AkariInago-no-MacBook-Air-2.local>
2016-10-21 15:51:03 +0900

    未知語処理なし

--- make_data.py
+++ make_data.py
@@ -1,7 +1,167 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import csv
+import codecs
 import sys
 import pickle
+import MeCab
+import mojimoji
+
+def analyse(text):
+    mecab = MeCab.Tagger() ## MeCabのインスタンス作成
+
+    node = mecab.parseToNode(text) ## 解析を実行
+    morphs = []
+    poses = []
+    while node:
+        # surface : 形態素の文字列情報
+        # feature : 素性情報
+        
+        """
+        #そのまま出力
+        print node.surface, node.feature
+        
+        """
+        
+        hinsi  = node.feature.split(",")[0]
+        hinsi1 = node.feature.split(",")[1]
+        genkei = node.feature.split(",")[6]
+        #print "[原形]",genkei,"\t","[品詞]",hinsi,"\t","[品詞細分類1]",hinsi1
+        poses.append(hinsi)
+        if (genkei == "*"):
+            morphs.append(node.surface)
+        else:
+            morphs.append(genkei)
+        node = node.next
+    poses.pop(0)
+    poses.pop()
+    morphs.pop(0)
+    morphs.pop()
+    return morphs, poses
+
+def make_train_data(morph, words, labels, pos):
+    features = []
+    print morph[0],pos[0]
+    j = 0
+    for i in range(0, len(morph)):
+            
+        features.append("ST_p." + pos[i])
+        features.append("ST_w." + morph[i])
+        features.append("ST_p_w." + pos[i] + morph[i])
+        if (i != len(morph) - 1):
+            features.append("Q0_p." + pos[i + 1])
+            features.append("Q0_w." + morph[i + 1])
+            features.append("Q0_p_w." + pos[i + 1] + morph[i + 1])
+        
+            features.append("ST_p_w_Q0_p_w." + pos[i] + morph[i] + "_" + pos[i + 1] + morph[i + 1])
+            features.append("ST_p_w_Q0_w_." + pos[i] + morph[i] +"_" + morph[i + 1])
+            features.append("ST_p_Q0_p." + pos[i] + "_" + pos[i + 1])
+            
+            if (i != len(morph) - 2):
+                features.append("Q1_p." + pos[i + 2])
+                features.append("Q1_w." + morph[i + 2])
+                features.append("Q1_p_w." + pos[i + 2] + morph[i + 2])
+                features.append("Q0_p_Q1_p." + pos[i + 1] + "_" + pos[i + 2])
+            
+                if (i != len(morph) - 3):
+                    features.append("Q0_p_Q1_p_Q2_p." + pos[i + 1] + "_" + pos[i + 2] + "_" + pos[i + 3])
+        if (words[j] == morph[i]):
+            features.append("\tR-" + labels[j] + "\n")
+            features.append("ST_p.")
+            features.append("ST_w." + labels[j])
+            features.append("ST_p_w." + labels[j])
+            if (i != len(morph) - 1):
+                features.append("ST_p_w_Q0_p_w." + labels[j] + "_" + pos[i + 1] + morph[i + 1])
+                features.append("ST_p_w_Q0_w_." + labels[j] + "_" + morph[i + 1])
+                features.append("ST_p_Q0_p." + labels[j] + "_" + pos[i + 1])
+            j += 1
+        features.append("\tS\n")
+    return features
+
+def make_feature(morph, words, labels, pos):
+    features = []
+    print morph[0],pos[0]
+    j = 0
+    for i in range(0, len(morph)):
+            
+        features.append("ST_p." + pos[i])
+        features.append("ST_w." + morph[i])
+        features.append("ST_p_w." + pos[i] + morph[i])
+        if (i != len(morph) - 1):
+            features.append("Q0_p." + pos[i + 1])
+            features.append("Q0_w." + morph[i + 1])
+            features.append("Q0_p_w." + pos[i + 1] + morph[i + 1])
+        
+            features.append("ST_p_w_Q0_p_w." + pos[i] + morph[i] + "_" + pos[i + 1] + morph[i + 1])
+            features.append("ST_p_w_Q0_w_." + pos[i] + morph[i] +"_" + morph[i + 1])
+            features.append("ST_p_Q0_p." + pos[i] + "_" + pos[i + 1])
+            
+            if (i != len(morph) - 2):
+                features.append("Q1_p." + pos[i + 2])
+                features.append("Q1_w." + morph[i + 2])
+                features.append("Q1_p_w." + pos[i + 2] + morph[i + 2])
+                features.append("Q0_p_Q1_p." + pos[i + 1] + "_" + pos[i + 2])
+            
+                if (i != len(morph) - 3):
+                    features.append("Q0_p_Q1_p_Q2_p." + pos[i + 1] + "_" + pos[i + 2] + "_" + pos[i + 3])
+        if (words[j] == morph[i]):
+            features.append("\n")
+            features.append("ST_p.")
+            features.append("ST_w." + labels[j])
+            features.append("ST_p_w." + labels[j])
+            if (i != len(morph) - 1):
+                features.append("ST_p_w_Q0_p_w." + labels[j] + "_" + pos[i + 1] + morph[i + 1])
+                features.append("ST_p_w_Q0_w_." + labels[j] + "_" + morph[i + 1])
+                features.append("ST_p_Q0_p." + labels[j] + "_" + pos[i + 1])
+            j += 1
+        features.append("\n")
+    return features
+
+def csv_read(filename, encoding):
+    """CSV ファイルを文字コードを指定して読み込む
+    """
+    with open(filename, "rU") as f:
+        csvfile = csv.reader(f)
+        rows = [[c.decode(encoding) for c in r] for r in csvfile]
+    return rows
+
+def rows_read(rows, words, labels, output_file):
+    """CSV ファイルを文字コードを指定して読み込む """
+    for row in rows:
+        morphemes = []
+        poses = []
+        features = []
+        morphemes, poses = analyse(mojimoji.han_to_zen(row[4]).encode("utf-8"))
+        features = make_train_data(morphemes, words, labels, poses)
+        for feature in features:
+            output_file.write(feature+",")
+
+def rows_read2(rows, words, labels, output_file):
+    """CSV ファイルを文字コードを指定して読み込む """
+    for row in rows:
+        morphemes = []
+        poses = []
+        features = []
+        morphemes, poses = analyse(mojimoji.han_to_zen(row[4]).encode("utf-8"))
+        features = make_feature(morphemes, words, labels, poses)
+        for feature in features:
+            output_file.write(feature+",")
+
+
+def make_labels(f):
+    ls = []
+    ws = []
+    for line in f:
+            #line.rstrip('rn')
+            labels = line.split(",")
+            print labels[1]
+            ws.append(labels[0])
+            l =labels[1].replace('\n','')
+            l =l.replace('\r','')
+            ls.append(l)
+    return ws, ls
+
 if __name__ == "__main__":
     args = sys.argv
     f = open(args[2], 'r')
@@ -9,23 +169,81 @@ if __name__ == "__main__":
     # make a rule
     if (args[1] == 'r'):
         rules = []
+        p = []
+        prod = []
         for line in f:
-            itemList = [l.decode('utf-8') for l in line[:-1].split('->')]
-            print itemList
-            rules.append(itemList)
+            line = line[:-1].split('->')
+            p.append(line[0])
+            prod.append(line[1])
+        print line[0],line[1]
+        rules.append(p)
+        rules.append(prod)
         f.close()
         pickle.dump(rules, open("rules.pickle", "wb"))
-        #rules = pickle.load(open("rules.pickle"))
     
     # make a dic
+    # fetature1, feature2,feature3
+    # → 1,2,3
     if (args[1] == 'd'):
         dic = {}
         num = 1
         for line in f:
-        features = line.split(",")
-        for feature in features:
-            if feature in dic :
+            line.rstrip("\n")
+            features = line.split(",")
+            for feature in features:
+                if feature in dic :
+                    continue
+                dic[feature] = num
+                print num, feature
+                num += 1
+        pickle.dump(dic, open("dic_data.pickle", "wb"))
+        
+    # make a train data
+    # fetature1, feature2,feature3\t0
+    # → 1,2,3, 
+    if (args[1] == 't'):
+        train = []
+        dic = pickle.load(open("dic_data.pickle"))
+        output_file = open("train_data.txt","w")
+        num = 1
+        for line in f:
+            t = []
+            line.rstrip("\n")
+            print line
+            line = line.split("\t")
+            if len(line) != 2:
                 continue
-            dic[feature] = num
-            num += 1
-        pickle.dump(rules, open("dic_data.pickle", "wb"))
+            features = line[0]
+            action = line[1]
+            action = action.replace('\n','')
+            action = action.replace('\r','')
+            #print features
+            #print action
+            features = features.split(",")
+            for feature in features:
+                if feature in dic :
+                    t.append(dic[feature])
+            t.append(action)
+            train.append(t)
+            for i in range(0, len(t) - 1):
+                output_file.write(str(t[i]) + ',')
+            output_file.write(str(t[len(t) - 1]) + '\n')
+        pickle.dump(train, open("train_data.pickle", "wb"))
+        
+    if (args[1] == 'f'):
+        encoding = 'sjis'
+        output_file = open("feature.txt","w")
+        # [["に,L"], [""],
+        labels_file = open(args[3], 'r')
+        words, labels = make_labels(labels_file)
+        csv_rows = csv_read(args[2], encoding)
+        rows_read2(csv_rows, words, labels, output_file)
+
+    if (args[1] == 'tr'):
+        encoding = 'sjis'
+        output_file = open("train.txt","w")
+        # [["に,L"], [""],
+        labels_file = open(args[3], 'r')
+        words, labels = make_labels(labels_file)
+        csv_rows = csv_read(args[2], encoding)
+        rows_read(csv_rows, words, labels, output_file)
--- parser.py
+++ parser.py
@@ -1,28 +1,188 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 """Train a shift-reduce parser, parse SDC from command"""
-import pickle
-import os
+import argparse
+import collections
+from itertools import chain
+import MeCab
+import mojimoji
 import nltk
 from nltk.classify import MaxentClassifier
-import collections
-import time
+import os
+import pickle
+import pycrfsuite
+import sklearn
+from sklearn.metrics import classification_report
+from sklearn.preprocessing import LabelBinarizer
 import sys
-import argparse
-import MeCab
+import time
+
+
+class CorpusReader(object):
+
+    def __init__(self, path):
+        with codecs.open(path, encoding='utf-8') as f:
+            sent = []
+            sents = []
+            for line in f:
+                if line == '\n':
+                    sents.append(sent)
+                    sent = []
+                    continue
+                morph_info = line.strip().split('\t')
+                sent.append(morph_info)
+        train_num = int(len(sents) * 0.9)
+        self.__train_sents = sents[:train_num]
+        self.__test_sents = sents[train_num:]
+
+    def iob_sents(self, name):
+        if name == 'train':
+            return self.__train_sents
+        elif name == 'test':
+            return self.__test_sents
+        else:
+            return None
+def is_hiragana(ch):
+    return 0x3040 <= ord(ch) <= 0x309F
+
+def is_katakana(ch):
+    return 0x30A0 <= ord(ch) <= 0x30FF
+
+def get_character_type(ch):
+    if ch.isspace():
+        return 'ZSPACE'
+    elif ch.isdigit():
+        return 'ZDIGIT'
+    elif ch.islower():
+        return 'ZLLET'
+    elif ch.isupper():
+        return 'ZULET'
+    elif is_hiragana(ch):
+        return 'HIRAG'
+    elif is_katakana(ch):
+        return 'KATAK'
+    else:
+        return 'OTHER'
+
+def get_character_types(string):
+    character_types = map(get_character_type, string)
+    character_types_str = '-'.join(sorted(set(character_types)))
+
+    return character_types_str
+
+def extract_pos_with_subtype(morph):
+    idx = morph.index('*')
+
+    return '-'.join(morph[1:idx])
+def word2features(sent, i):
+    word = sent[i][0]
+    chtype = get_character_types(sent[i][0])
+    postag = extract_pos_with_subtype(sent[i])
+    features = [
+        'bias',
+        'word=' + word,
+        'type=' + chtype,
+        'postag=' + postag,
+    ]
+    if i >= 2:
+        word2 = sent[i-2][0]
+        chtype2 = get_character_types(sent[i-2][0])
+        postag2 = extract_pos_with_subtype(sent[i-2])
+        iobtag2 = sent[i-2][-1]
+        features.extend([
+            '-2:word=' + word2,
+            '-2:type=' + chtype2,
+            '-2:postag=' + postag2,
+            '-2:iobtag=' + iobtag2,
+        ])
+    else:
+        features.append('BOS')
+
+    if i >= 1:
+        word1 = sent[i-1][0]
+        chtype1 = get_character_types(sent[i-1][0])
+        postag1 = extract_pos_with_subtype(sent[i-1])
+        iobtag1 = sent[i-1][-1]
+        features.extend([
+            '-1:word=' + word1,
+            '-1:type=' + chtype1,
+            '-1:postag=' + postag1,
+            '-1:iobtag=' + iobtag1,
+        ])
+    else:
+        features.append('BOS')
+
+    if i < len(sent)-1:
+        word1 = sent[i+1][0]
+        chtype1 = get_character_types(sent[i+1][0])
+        postag1 = extract_pos_with_subtype(sent[i+1])
+        features.extend([
+            '+1:word=' + word1,
+            '+1:type=' + chtype1,
+            '+1:postag=' + postag1,
+        ])
+    else:
+        features.append('EOS')
+
+    if i < len(sent)-2:
+        word2 = sent[i+2][0]
+        chtype2 = get_character_types(sent[i+2][0])
+        postag2 = extract_pos_with_subtype(sent[i+2])
+        features.extend([
+            '+2:word=' + word2,
+            '+2:type=' + chtype2,
+            '+2:postag=' + postag2,
+        ])
+    else:
+        features.append('EOS')
+
+    return features
+
+
+def sent2features(sent):
+    return [word2features(sent, i) for i in range(len(sent))]
+
+
+def sent2labels(sent):
+    return [morph[-1] for morph in sent]
+
+
+def sent2tokens(sent):
+    return [morph[0] for morph in sent]
+
+def bio_classification_report(y_true, y_pred):
+    lb = LabelBinarizer()
+    y_true_combined = lb.fit_transform(list(chain.from_iterable(y_true)))
+    y_pred_combined = lb.transform(list(chain.from_iterable(y_pred)))
+
+    tagset = set(lb.classes_) - {'O'}
+    tagset = sorted(tagset, key=lambda tag: tag.split('-', 1)[::-1])
+    class_indices = {cls: idx for idx, cls in enumerate(lb.classes_)}
+
+    return classification_report(
+        y_true_combined,
+        y_pred_combined,
+        labels = [class_indices[cls] for cls in tagset],
+        target_names = tagset,
+    )
+
+class grammer:
+    def __init__(self):
+        self.p = []
+        self.prod = []
 
 class Item(object):
     """"Class representing a state of a parser"""
     def __init__(self, stack, queue, pos, features, score, res):
-        self.stack = stack
-        self.queue = queue
+        self.stack = stack[:]
+        self.queue = queue[:]
         self.pos = pos
-        self.features = features[:]
         self.score = score
         self.res = res[:]
-
-        # Features are constructed in every state
+        self.features = features[:]
         self.features.append(self.construct_features(pos, stack, queue))
+        # Features are constructed in every state
+        #self.features.append(self.construct_features(pos, stack, queue))
 
     def construct_features(self, pos, stack, queue):
         """Construct features for current state of the parser
@@ -39,10 +199,11 @@ class Item(object):
         # Properties of stack top
         if len(stack) != 0 :
             head = stack[-1]
-            # HeadがSDCラベルの場合posは多分何もつかない
-            features.append("ST_p." + pos[head])
-            features.append("ST_w." + head)
-            features.append("ST_p_w." + pos[head] + head)
+            if (pos.get(head)): 
+                # HeadがSDCラベルの場合posは多分何もつかない
+                features.append("ST_p." + pos[head])
+                features.append("ST_w." + head)
+                features.append("ST_p_w." + pos[head] + head)
 
         # If stack is empty
         else:
@@ -51,26 +212,29 @@ class Item(object):
         # Properties of queue head
         if len(queue) != 0 :
             next = queue[0]
-            features.append("Q0_p." + pos[next])
-            features.append("Q0_w." + next)
-            features.append("Q0_p_w." + pos[next] + next)
+            if (pos.get(next)):
+                features.append("Q0_p." + pos[next])
+                features.append("Q0_w." + next)
+                features.append("Q0_p_w." + pos[next] + next)
 
-            # Conjunction of stack top and queue head
-            if len(stack) != 0:
-                head = stack[-1]
-                features.append("ST_p_w_N0_p_w." + pos[head] + head + "_" + pos[next] + next)
-                features.append("ST_p_w_N0_w_." + pos[head] + head + "_" + next)
-                features.append("ST_p_N0_p."+pos[head] + "_" + pos[next])
+                # Conjunction of stack top and queue head
+                if len(stack) != 0:
+                    head = stack[-1]
+                    if (pos.get(head)):
+                        features.append("ST_p_w_Q0_p_w." + pos[head] + head + "_" + pos[next] + next)
+                        features.append("ST_p_w_Q0_w_." + pos[head] + head + "_" + next)
+                        features.append("ST_p_Q0_p."+pos[head] + "_" + pos[next])
 
         # Properties of next item in queue
         if len(queue) > 1:
             next = queue[1]
-            features.append("N1_p." + pos[next])
-            features.append("N1_w." + next)
-            features.append("N1_p_w." + pos[next] + next)
-            features.append("N0_p_N1_p." + pos[queue[0]] + "_" + pos[next])
-            if len(queue) > 2:
-                features.append("N0_p_N1_p_N2_p."+pos[queue[0]]+"_"+pos[next]+"_"+pos[queue[2]][:-1])
+            if (pos.get(next)):
+                features.append("Q1_p." + pos[next])
+                features.append("Q1_w." + next)
+                features.append("Q1_p_w." + pos[next] + next)
+                features.append("Q0_p_Q1_p." + pos[queue[0]] + "_" + pos[next])
+                if len(queue) > 2:
+                    features.append("Q0_p_Q1_p_Q2_p."+pos[queue[0]]+"_"+pos[next]+"_"+pos[queue[2]])
 
         return features
 
@@ -80,11 +244,13 @@ def shift(item, p, prod):
     item -- current state of the parser
     """
     items = []
-    if (q != []) :
+    if (item.queue != []) :
         q = item.queue[0]
         s = item.stack[:]
         s.append(q)
-    items.append(Item(s, item.queue[1:], item.pos,  item.feature, item.res))
+        items.append(Item(s, item.queue[1:], item.pos, item.features, item.score, item.res)) 
+    else :
+        items.append(Item([], [], item.pos, item.features, item.score, item.res))
     return items
 
 def reduce(item, p, prod):
@@ -95,26 +261,35 @@ def reduce(item, p, prod):
     items = []
     ts = ""
     s = item.stack[:]
-    if (isAlphabet(s[0]) == False && s[0] != '('):
-        while not (isAlphabet(s[0])) :
+    item0 = item
+    if (isAlphabet(s[-1]) == False and s[-1][-1] != ')'):
+        i = len(s) - 1
+        while (isAlphabet(s[i]) ==False and s[i][-1] != ')') :
             ts = s[-1] + ts
             s.pop()
-            if (s is None) :
+            i -= 1
+            if (i < 0) :
                 break
     else :
-        if (len(s) > 2) :
+        if (len(s) > 1) :
             ts = s[-2] + s[-1]
             s.pop()
             s.pop()
         else :
             ts = "X"
+    flag = 0
+
     for i in range (0, len(p)) :
         if (ts == prod[i]):
+            flag = 1
             s.append(p[i])
-            res = item.res[:]
-            res = make_sdc(res, i)
-            items.append(Item(s, item.queue, item.pos,  item.feature, item.res))
+            res = make_sdc(item.res, i)
+            
+            items.append(Item(s, item.queue, item.pos,  item.features, item.score, res))
             s.pop()
+            item.res.pop()
+    if flag == 0:
+        items.append(Item([], item.queue, item.pos, item.features, item.score, item.res))
     return items
 
 def shift_reduce(sentence, pos, p, prod, size, dic_data, classify):
@@ -131,35 +306,43 @@ def shift_reduce(sentence, pos, p, prod, size, dic_data, classify):
 
     # Construct the starting item
     start_item = type('TestItem',  (),  {})()
-    start_item.queue = range(len(sentence))
+    start_item.queue = sentence
     start_item.stack = []
     start_item.features = []
-    start_item = shift(start_item)
+    start_item.pos = pos
     start_item.score = 1
+    start_item.flag = 0
+    start_item.res = []
+    start_item = shift(start_item, p, prod)[0]
     deque.append(start_item)
 
     result = None
     score = 0
+    candidate_lst = []
     while deque:
         lst = []
         for item in deque:
             # Perform all possible actions on item taken from agenda
             for i in range(len(actions)):
                 new_items = actions[i](item, p, prod)
-                for new_item in new_items :
-                    if new_item is None:
-                        continue
-                    new_score = compute_score(new_item, classify, dic_data)
-                    new_item.score = new_score
+                if (len(new_items) != 0):
+                    for new_item in new_items :
+                        if new_item.stack == []:
+                            continue
+                        new_score = compute_score(new_item, dic_data, classify)
+                        new_item.score = new_score
 
-                    # Item is finished; compare it to current candidate
-                    s = new_item.stack
-                    if s[0] == "E" && len(s) == 1: 
-                        if result is None or new_score > score:
-                            result = new_item
-                            score = new_score
+                        # Item is finished; compare it to current candidate
+                        if new_item.stack[0] == "E" and len(new_item.stack) == 1: 
+                            #print "finish!"
+                            if result is None or new_score > score:
+                                result = new_item
+                                score = new_score
+                                #print result.res
                         else:
                             lst.append(new_item)
+                            if (new_item.queue == []):
+                                candidate_lst.append(new_item)
         # Only best B items are chosen as the new agenda
         lst.sort(key=lambda x: x.score,  reverse=True)
         deque = lst[:size]
@@ -176,27 +359,41 @@ def parse(text):
     node = mecab.parseToNode(text) ## 解析を実行
     ips = []
     poses = {}
+    node = node.next
     while node:
         # surface : 形態素の文字列情報
         # feature : 素性情報
-        ips.append(surface)
-        poses[surface]=feature
+        ips.append(node.surface)
+        poses[node.surface]=node.feature.split(",")[0]
         node = node.next
+    ips.pop()
     return ips, poses
+
+def make_dinitial():
+    dinitial={}
+    # The number of features is 4627.
+    #for i in range(1,4628):
+    for i in range(1,5):
+    #for i in range(1,56):
+        if not i in dinitial:
+            dinitial[str(i)] = int()
+            dinitial[str(i)] = 0
+    return dinitial
 
 def train(train_data, algorithm, max_iter, dic_data):
     train = []
+    dinitial = make_dinitial()
     for t in train_data:
-        dd = dic_data.copy()
+        dd = dinitial.copy()
         length = len(t)
         #There is a space before label, so it is removed by lstrip().
-        label = t[length]
-        for token in t[0:length-1]:
+        label = t[-1]
+        for token in t[0:-2]:
             dd[token]=1
         tp = (dd,label) # label shift or reduce1 or reduce2...
         train.append(tp)
     try:
-        classifier = nltk.classify.MaxentClassifier.train(train, algorithm, trace=0, max_iter)
+        classifier = nltk.classify.MaxentClassifier.train(train, algorithm, trace=0, max_iter=1)
     except Exception as e:
         print('Error: %r' % e)
         return
@@ -209,35 +406,41 @@ def compute_score(item, classifier, dic_data):
     weights -- trained model
     index -- desired class
     """
+    dinitial = make_dinitial()
     new_score = item.score
-    dd = dic_data.copy()
-    for feat in item.features:
-        if feat in dic_data:
-            dd[dic_data[feat]] = 1
+    dd = dinitial.copy()
+    #print item.stack
+    for feature in item.features:
+        for feat in feature:
+            if feat in dic_data:
+                #print feat
+                dd[dic_data[feat]] = 1
 
     featureset = (dd)
     pdist = classifier.prob_classify(featureset)
+    
     """ shift S
         Reduce-O R-O
-        
     """
-    act_list = []
+
+    act_list = ["S", "R-O", "R-L", "R-(O\L)", "R-O\(O/O)", "R-L\(O/O)", "R-(O/O)", "R-(L\E)",  "R-(O\E)"]
     b_score = pdist.prob(act_list[0])
     b_action = act_list[0]
 
-    for act in actlist:
+    for act in act_list:
         if (pdist.prob(act) > b_score):
             b_score = pdist.prob(act)
             b_action = act
-    new_score *= pdist.prob(b_action) * 10
-    return Item(s, item.queue, item.pos, item.feature, new_score, item.res)
+    new_score *= (pdist.prob(b_action) * 10.0)
+    return new_score
 
 def make_sdc(res, i):
-    if not isAlphabet(prod[i][0]):
+    #print p[i],prod[i]
+    if isAlphabet(prod[i][0]) == False and prod[i][0] != "(" :
         res.append("(" + p[i] + " " + prod[i] + " )")
     else:
-        ans.insert(0, "( " + p[i])
-        ans.append(" )")
+        res.insert(0, "( " + p[i])
+        res.append(" )")
     return res
 
 def output_res(res):
@@ -249,16 +452,22 @@ if __name__ == "__main__":
     # Mode for training a log-linear model
     #[[1:"feature1", 2:"feature2", 3:"feature3"]]
     dic_data = pickle.load(open("dic_data.pickle"))
-    #[[feature1, feature2, feature3, feature4, S],[..., R-O],[..., R-L]]
+    #[[1, 2, 3, 4, S],[..., R-O],[..., R-L]]
     train_data = pickle.load(open("train_data.pickle"))
-    classifier = train(train_data, "iis", 1)
+    classifier = train(train_data, "iis", 1, dic_data)
 
     # Mode for parsing all questions and evaluating model
     sys.stdout.write("Enter Input:")
     input = raw_input()
-    phrases, pos = parse(input.encode("utf-8"))
+    phrases, pos = parse(input)
     p, prod = pickle.load(open("rules.pickle"))
     beam_size = 20
     res = shift_reduce(phrases, pos, p, prod, beam_size, classifier, dic_data)
-    output_res(res)
 
+    #SDCが作られなかった場合未知語の処理をする
+    if (res is None):
+        #predict_word()
+        #print res
+        print "Not_found"
+    else:
+        output_res(res.res)
